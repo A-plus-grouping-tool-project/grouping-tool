e66abb9c663d5c38ff33386d10f7688eaabe3ca1
elli <elli.hurme@hotmail.com>
2018-11-05 18:01:00 +0200

    Add new template for teacher view

--- project/app/urls.py
+++ project/app/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', views.mainPage, name='mainPage'),
     path('query/', views.query, name='query'),
     path('students_from_course/', apiviews.students_from_course, name='students_from_course'),
-    path('group_students/', groupviews.group_students, name='group_students')
+    path('group_students/', groupviews.group_students, name='group_students'),
+    path('teacher/', views.teacherView, name='teacherView'),
 ]
