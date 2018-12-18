e9561156389a2193276265b0d468446fb43f6e5e
Aku Lindgren <yksi@funktio.ug>
2018-12-18 19:39:38 +0200

    Update models
    fixup! Update models
    
    (cherry picked from commit 8c4d1db204da1bf1c86071b250a5ae3f8ebfaf6f)

--- project/app/models.py
+++ project/app/models.py
@@ -1,11 +1,30 @@
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
 
+SEP = ', '
 
 # Create your models here.
+class Student(models.Model):
+    id = models.IntegerField(primary_key=True)
+    username = models.CharField(max_length=50) 
+    student_id = models.CharField(max_length=10)
+    email = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.id + SEP + self.username + SEP + self.student_id +  SEP + self.email
+
 class Group(models.Model):
-    name = models.CharField(max_length=200)
-    maxsize = models.IntegerField()
-    course_code = models.CharField(max_length=15)
-    type = models.CharField(max_length=50)
-    students = ArrayField(models.IntegerField())
+    group_id = models.IntegerField()
+    course_code = models.CharField(max_length=10)
+    students = models.ManyToManyField(Student)
+    group_type = models.IntegerField()
+
+    def __str__(self):
+        return self.group_id + SEP + self.course_code + SEP + self.group_type
+
+
+class Type(models.Model):
+    type_id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=50)
+    max_size = models.IntegerField()
+    
