a812455c65ce873ad50a3060c3f3787bc0652cd0
Jaakko Kankaansivu <jaakkok11@gmail.com>
2018-12-20 17:01:38 +0200

    Modify requirements and urls

--- project/app/urls.py
+++ project/app/urls.py
@@ -1,16 +1,16 @@
-from django.urls import path
+from django.conf.urls import url
 
 from . import views
-from apirequests import views as apiviews
-from grouper import views as groupviews
+from ..apirequests import views as apiviews
+from ..grouper import views as groupviews
 
 urlpatterns = [
-    path('', views.mainPage, name='mainPage'),
-    path('query/', views.query, name='query'),
-    path('/', apiviews.students_from_course, name='students_from_course'),
-    path('group_students/<int:course_id>', views.group_students, name='group_students'),
-    path('teacher/', views.teacherView.as_view(), name='teacherView'),
-    path('student/', views.studentView.as_view(), name='studentView'),
-    path('edit_group/<int:pk>', views.edit_group.as_view(), name='edit_group'),
-    path('view_group/<int:pk>', views.view_group.as_view(), name='view_group'),
+    url(r'^$', views.mainPage, name='mainPage'),
+    url(r'^query/', views.query, name='query'),
+    url(r'^students_from_course/', apiviews.students_from_course, name='students_from_course'),
+    url(r'^group_students/', groupviews.group_students, name='group_students'),
+    url(r'^teacher/', views.teacherView.as_view(), name='teacherView'),
+    url(r'^student/', views.studentView.as_view(), name='studentView'),
+    url(r'^edit_group/<pk>', views.edit_group.as_view(), name='edit_group'),
+    url(r'^view_group/<pk>', views.view_group.as_view(), name='view_group'),
 ]
