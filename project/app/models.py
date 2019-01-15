d97e054a672b82080007c82f3118e47d45031739
elli <elli.hurme@hotmail.com>
2019-01-15 20:12:10 +0200

    Add str() to__str__ function.

--- project/app/models.py
+++ project/app/models.py
@@ -6,12 +6,12 @@ SEP = ', '
 # Create your models here.
 class Student(models.Model):
     id = models.IntegerField(primary_key=True)
-    username = models.CharField(max_length=50) 
+    username = models.CharField(max_length=50)
     student_id = models.CharField(max_length=10)
     email = models.CharField(max_length=50)
-    
+
     def __str__(self):
-        return self.id + SEP + self.username + SEP + self.student_id +  SEP + self.email
+        return str(self.id) + SEP + self.username + SEP + self.student_id +  SEP + self.email
 
 class Group(models.Model):
     group_id = models.IntegerField()
@@ -20,11 +20,10 @@ class Group(models.Model):
     group_type = models.IntegerField()
 
     def __str__(self):
-        return self.group_id + SEP + self.course_code + SEP + self.group_type
+        return str(self.group_id) + SEP + self.course_code + SEP + str(self.group_type)
 
 
 class Type(models.Model):
     type_id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=50)
     max_size = models.IntegerField()
-    
