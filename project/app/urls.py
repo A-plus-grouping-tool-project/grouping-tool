7fccc4eda132da9cc9deaa1b08543baa660f4cfa
Lauri Suomela <lauri.suomela@outlook.com>
2019-02-12 11:07:31 +0200

    Add modal for viewing groups as a student
    Add student view with functionality to view groups and their members, also added favicon and changed header colour.

--- project/app/urls.py
+++ project/app/urls.py
@@ -10,5 +10,7 @@ urlpatterns = [
     path('students_from_course/', apiviews.students_from_course, name='students_from_course'),
     path('group_students/', groupviews.group_students, name='group_students'),
     path('teacher/', views.teacherView.as_view(), name='teacherView'),
-    path('edit_group/<int:pk>', views.edit_group.as_view(), name='edit_group')
+    path('student/', views.studentView.as_view(), name='studentView'),
+    path('edit_group/<int:pk>', views.edit_group.as_view(), name='edit_group'),
+    path('view_group/<int:pk>', views.view_group.as_view(), name='view_group')
 ]
