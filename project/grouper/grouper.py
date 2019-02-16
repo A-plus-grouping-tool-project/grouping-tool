7219457097ea6db387bfe4a67696b8b5103933df
Aku Lindgren <aku.lindgren@neuroeventlabs.com>
2019-02-16 15:29:34 +0200

    WIP: grouping logic

--- project/grouper/grouper.py
+++ project/grouper/grouper.py
@@ -1,17 +1,20 @@
 import json
 from apirequests import views
 from app.models import Student, Group, Course
+from app import courses, students
 from . import csv_maker
 
 #wip
 def group_students(group_size =  3, course_id = 1):
     resp = views.students_from_course(course_id)
     data = json.loads(resp.content)
+    course = courses.add_course(course_id)
     group_id_iterator = 1
     group = init_group(course_id, group_id_iterator)
     for i in range(data['count']):
+        group.save()
         student = data['results'][i]
-        student_object = student_to_database(student, course_id)
+        student_object = students.add_student(student, course_id)
         group.students.add(student_object)
         if i % group_size == 0:
             group.save()
@@ -25,15 +28,16 @@ def init_group(course_id, group_id):
     return group
     
 def student_to_database(student_object, course_id = -1):
+
     student = Student()
 
     student.student_id = student_object['id']
     student.username = student_object['username']
     student.email = student_object['email']
-    if(student_object['student_id'] != None):
+    if student_object['student_id'] != None:
         student.student_id = student_object['student_id']
     student.save()
-    if (course_id != -1):
+    if course_id != -1:
         course = Course()
         course.student = student_object['id']
         course.id = course_id
